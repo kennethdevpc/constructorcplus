46e46ee0887cdb20d8ea0026b7d27a3736b80b12
Kenneth E. Puliche C <kennethdevpc@gmail.com>
2024-12-06 11:41:07 -0500

    palabra clave const

---
+++ first/keyconst.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int main (){
+  const double PI = 3.14;
+
+  double radio = 5.0;
+  double area = PI * radio * radio;
+
+  std::cout << "La circunferencia es: " << area << std::endl;
+}
