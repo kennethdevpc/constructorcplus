7245b075b53f0cd9dc0b096ba19f06137dfe9fa4
Kenneth E. Puliche C <kennethdevpc@gmail.com>
2024-12-07 09:31:32 -0500

    calculo hipotenusa

---
+++ first/20/12-hipo.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+  double a;
+  double b;
+  std::cout << "Ingrese el valor de a: ";
+  std::cin >> a;
+  std::cout << "Ingrese el valor de b: ";
+  5;
+  std::cin >> b;
+  double c = sqrt(pow(a,2) + b * b);
+  std::cout<< "La hipotenusa es: " << c << std::endl;
+  return 0;
+}
